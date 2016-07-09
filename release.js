3fd52c38018cccde159b3438cc8fe82aead03fb8
Steffan <steffan@schiewe.biz>
2016-07-09 09:20:26 +0200

    update

--- release.js
+++ release.js
@@ -1,16 +1,14 @@
 var replace = require('replace-in-file');
 var package = require('./package.json');
 
-console.log(package.version);
+replace({
+  files: "bower.json",
+  replace: /("version"\s*:\s*")\d+\.\d+\.\d+("\s*,)/g,
+  with: "$1" + package.version + "$2"
+});
 
-// replace({
-//   files: "bower.json",
-//   replace: /("version"\s*:\s*")\d+\.\d+\.\d+("\s*,)/g,
-//   with: "$1" + version + "$2"
-// });
-
-// replace({
-//   files: "README.md",
-//   replace: /(\/|@)\d+\.\d+\.\d+/g,
-//   with: "$1" + version
-// });
+replace({
+  files: "README.md",
+  replace: /(\/|@)\d+\.\d+\.\d+/g,
+  with: "$1" + package.version
+});
